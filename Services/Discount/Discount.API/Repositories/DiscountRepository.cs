9f512150579a32e4ef67ffb4ed3b4da7fa02a47d
Tuan Tran <tranquoctuanitsp@gmail.com>
2021-08-01 21:27:22 +0700

    create Discount API

---
+++ Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,37 @@
+﻿using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Coupon> GetDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
